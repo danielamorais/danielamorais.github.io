635ed12c72915afae710403f2ef2d64a61c2731b
Daniela Morais <daniela.morais@stoom.com.br>
2017-12-24 00:22:19 -0200

    Gerar novo site estático com testes

---
+++ test/localhostaddress.py
@@ -0,0 +1,37 @@
+import os
+
+filesNameList = []
+contains = None
+
+def checkFile(pathname):
+    with open(pathname, "r") as ins:
+        cont = 0
+        global contains
+        for line in ins:
+            cont += 1
+            if "localhost:2368" in line:
+                contains = True
+                print "The file " + pathname + " contains localhost in " + str(cont) + " line"
+
+def containsLocalHost():
+    # Open temp file in ./test and return to /static
+    with open("output", "r") as ins:
+        filesNameList = []
+        for line in ins:
+            filesNameList.append(line)
+
+    # Check one file per time
+    for i in range(len(filesNameList)):
+        if filesNameList[i].count("/") == 1:
+            filesNameList[i] = filesNameList[i].replace("./", "").replace("\n", "")
+            pathname = os.path.join("..", filesNameList[i])
+            checkFile(pathname)
+        else:
+            filesNameList[i] = filesNameList[i].replace("\n", "")
+            pathname = os.path.join("." + filesNameList[i])
+            checkFile(pathname)
+    #Check sitemap.xml
+    pathname = os.path.join("..", "sitemap.xml")
+    checkFile(pathname)
+    return contains
+
